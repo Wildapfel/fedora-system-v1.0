ada3db763b9843e860e7b72c8d1a8d067e488648
Wildapfel <maxpetzold@yahoo.de>
2026-06-06 14:46:30 +0200

    updated background img logic

--- gnome-tweaks/gsetting_tweaks.sh
+++ gnome-tweaks/gsetting_tweaks.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.background picture-uri "$PWD/gnome-tweaks/background-img/stone_beach_aerial_green"
-gsettings set org.gnome.desktop.background picture-uri-dark "$PWD/gnome-tweaks/background-img/stone_beach_aerial_green_ocean"
+WALLPAPER_DIR="$HOME/Pictures/wallpaper"
+BACKGROUND_DIR="$PWD/gnome-tweaks/background-img"
+BACKGROUND_NAME="stone_beach_aerial_green_ocean"
 
+mkdir -p $WALLPAPER_DIR
+mv "$BACKGROUND_DIR/BACKGROUND_NAME" "$WALLPAPER_DIR/$BACKGROUND_NAME"
+
+gsettings set org.gnome.desktop.background picture-uri "$WALLPAPER_DIR/$BACKGROUND_NAME"
+gsettings set org.gnome.desktop.background picture-uri-dark "$WALLPAPER_DIR/$BACKGROUND_NAME"
