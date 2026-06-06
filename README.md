c0dba500a71e81854e8704c2b5f1398f762a2383
Wildapfel <maxpetzold@yahoo.de>
2026-06-06 14:35:01 +0200

    added the background section

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-hello world
+*quick notes*
+
+## sushi 
+- for what ever reasons i must install sushi into os layer, but uninstall the flatpak 
+  alternative (org.gnome.NautilusPreview) -> i belvie that was the bug !
+
+
---
+++ gnome-tweaks/gsetting_tweaks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background picture-uri "$PWD/gnome-tweaks/background-img/stone_beach_aerial_green"
+gsettings set org.gnome.desktop.background picture-uri-dark "$PWD/gnome-tweaks/background-img/stone_beach_aerial_green_ocean"
+
