9eb9cf4a65f52587cbc4b74db1d87820b223cb35
Wildapfel <maxpetzold@yahoo.de>
2026-06-07 09:50:18 +0200

    quick fix

--- external-tools/cheat/install.sh
+++ external-tools/cheat/install.sh
@@ -2,5 +2,5 @@
 
 PATH_DIR="$HOME/.local/bin"  # or another directory on your $PATH
 mkdir -p "$PATH_DIR"
-curl https://cht.sh/:cht.sh > "$PATH_DIR/cheat"
-chmod +x "$PATH_DIR/cheat"
+curl https://cht.sh/:cht.sh > "$PATH_DIR/cheat.sh"
+chmod +x "$PATH_DIR/cheat.sh"
